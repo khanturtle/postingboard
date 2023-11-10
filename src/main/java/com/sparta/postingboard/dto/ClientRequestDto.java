c77627934f25f6221d7b1a93d9cbfc6c90173d4d
khanturtle <chojunho0310@naver.com>
2023-11-10 22:40:44 +0900

    ADD - @Valid 어노테이션 추가

--- src/main/java/com/sparta/postingboard/dto/ClientRequestDto.java
+++ src/main/java/com/sparta/postingboard/dto/ClientRequestDto.java
@@ -1,9 +1,19 @@
 package com.sparta.postingboard.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class ClientRequestDto {
+    @NotNull
+    @Size(min=4,max=10)
+    @Pattern(regexp = "^[a-z0-9]*$")
     private String username;
+
+    @NotNull
+    @Size(min=8,max=15)
+    @Pattern(regexp = "^a-zA-Z0-9*$")
     private String password;
 }
